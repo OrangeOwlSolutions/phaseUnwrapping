1f670ff04477f58309e77706acad3a383e1a7343
Orange Owl <orangeowlsolutions@gmail.com>
2016-07-21 10:17:53 +0200

    Create phaseUnwrapping.cuh

---
+++ phaseUnwrapping.cuh
@@ -0,0 +1,7 @@
+#ifndef PHASEUNWRAPPING_CUH
+#define PHASEUNWRAPPING_CUH
+
+template<class T> void unwrap1D_host(T * __restrict, const int);
+template<class T> void unwrap1D_device(T * __restrict__, const int);
+
+#endif
